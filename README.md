64c609cb397dcd3170bc0ae3a88d881d82e7fbe8
Daniel Estupe <estupe.gonzalez@gmail.com>
2019-07-23 09:31:59 -0600

    The Vidly's first version. Data stored in array.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Little Practice with NodeJS
+
+It's a simple API to practice with NodeJS. 
+The API is called Vidly and contains controllers to manage CRUD actions.
---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const Joi = require('joi');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const genres = [
+    { id: 1, name: 'Terror' }
+];
+
+app.get('/api/genres', (req, res) => {
+    res.send(genres);
+});
+
+app.get('/api/genres/:id', (req, res) => {
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send('El genero no se ha encontrado');
+    res.send(genre);
+});
+
+app.post('/api/genres', (req, res) => {
+    const { error } = validateGenre(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    };
+    genres.push(genre);
+    res.send(genre);
+});
+
+app.put('/api/genres/:id', (req, res) => {
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send("El género con el ID proporcionado no fue encontrado.");
+
+    const { error } = validateGenre(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    genre.name = req.body.name;
+    res.send(genre);
+});
+
+app.delete('/api/genres/:id', (req, res) => {
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send("El género con el ID proporcionado no fue encontrado.");
+
+    const index = genres.indexOf(genre);
+    genres.splice(index, 1);
+
+    res.send(genre);
+});
+
+function validateGenre(genre) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(genre, schema);
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Escuchando en el puerto ${port}`));
